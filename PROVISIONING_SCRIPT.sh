c4c36e5013f9ca9298023db03d596c66e87bb886
digilateraldev <digilateraldev@gmail.com>
2025-11-04 12:06:49 +0530

    Add provisioning script for project setup

---
+++ PROVISIONING_SCRIPT.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/digilateraldev/ray-of-light-vast.git
+
+# Change to the project directory
+cd ray-of-light-vast
+
+# Install dependencies
+sudo apt update
+sudo apt install -y imagemagick sox
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Source the Rust environment variables
+source $HOME/.cargo/env
+
+# Install Python dependencies
+pip install -r requirements_new.txt
+pip install -U protobuf
+
+echo "Setup complete!"
