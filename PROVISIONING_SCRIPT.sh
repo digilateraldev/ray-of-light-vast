f704ba1b4c7e8e2c621a2c1556cc6934261253de
digilateraldev <digilateraldev@gmail.com>
2025-11-05 11:18:09 +0530

    Update Rust installation command in provisioning script

--- PROVISIONING_SCRIPT.sh
+++ PROVISIONING_SCRIPT.sh
@@ -14,7 +14,7 @@ sudo apt update
 sudo apt install -y imagemagick sox
 
 # Install Rust
-echo | curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # Source the Rust environment variables
 source $HOME/.cargo/env
