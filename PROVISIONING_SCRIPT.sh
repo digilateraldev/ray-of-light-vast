3352969aa30fe2169256e2f0e866a56aca3a6cdd
digilateraldev <digilateraldev@gmail.com>
2025-11-06 19:15:33 +0530

    Update provisioning script to change directory before installs
    Changed directory to 'ray-of-light-vast/' before installing Rust and Python dependencies.

--- PROVISIONING_SCRIPT.sh
+++ PROVISIONING_SCRIPT.sh
@@ -14,13 +14,14 @@ sudo apt update
 sudo apt install -y imagemagick sox
 
 # Install Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+cd ray-of-light-vast/ && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # Source the Rust environment variables
-source $HOME/.cargo/env
+cd ray-of-light-vast/ && source $HOME/.cargo/env
 
 # Install Python dependencies
-pip install -r requirements_new.txt
-pip install -U protobuf
+
+cd ray-of-light-vast/ && python3 -m pip install -r requirements_new.txt
+cd ray-of-light-vast/ && python3 -m pip install -U protobuf
 
 echo "Setup complete!"
