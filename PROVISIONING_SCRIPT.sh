202b822ae31359ab8ddf944d3c0134b071fdf72e
digilateraldev <digilateraldev@gmail.com>
2025-11-08 13:59:20 +0530

    Fix directory paths in provisioning script

--- PROVISIONING_SCRIPT.sh
+++ PROVISIONING_SCRIPT.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 #Change Directory
-cd /workspace
+cd /workspace/
+
+# Activate the main virtual environment
+. /venv/main/bin/activate
 
 # Clone the repository
 git clone https://github.com/digilateraldev/ray-of-light-vast.git
 
 # Change to the project directory
-cd ray-of-light-vast
+cd /workspace/ray-of-light-vast/
 
 # Install dependencies
 sudo apt update
@@ -15,7 +18,7 @@ sudo apt install -y imagemagick sox cargo
 
 # Install Python dependencies
 
-cd ray-of-light-vast/ && python3 -m pip install -r requirements_new.txt
-cd ray-of-light-vast/ && python3 -m pip install -U protobuf
+cd /workspace/ray-of-light-vast/ && python3 -m pip install -r requirements_new.txt
+cd /workspace/ray-of-light-vast/ && python3 -m pip install -U protobuf
 
 echo "Setup complete!"
